d45ebe140a727ec8046030f0ecbfebe0e51b723d
max <max@administrators-Mac-mini-2.local>
2018-12-13 17:54:16 +0700

    unit test snapshot card

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ To run the project:
 1. Clone this repo
 2. Run `npm install`
 3. Run `npm start`
+
+
+Using Unit test:
+1. Run `npm test` for running all test (*.spec.js)
+2. Run `npm test -- --coverage` for view report tester
--- src/components/__tests__/Card.spec.js
+++ src/components/__tests__/Card.spec.js
@@ -2,11 +2,15 @@ import { shallow } from 'enzyme'
 import Card from '../Card'
 import React from 'react'
 
-describe('',() => {
+describe('Card Component unit test', () => {
     it('expect to render Cards components', () => {
         // check total components render in Cards
-        // expect(shallow(<Card/>).length).toEqual(1)
-        expect(shallow(<Card/>)).toMatchSnapshot()
+        expect(shallow(<Card/>).length).toEqual(1)
         
+    })
+
+    it('should to match component card with snapshot', () => {
+        // will matching component Card with Card Snapshot
+        expect(shallow(<Card/>)).toMatchSnapshot()
     })
 })
