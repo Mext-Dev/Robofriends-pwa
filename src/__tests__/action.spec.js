6c9d7b5dbc39ee24257ff39687ee92855ef297b7
max <max@administrators-Mac-mini-2.local>
2018-12-19 11:43:39 +0700

    unit test action redux

--- src/__tests__/action.spec.js
+++ src/__tests__/action.spec.js
@@ -9,35 +9,35 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 import {
-    CHANGE_SEARCHFIELD,
-    REQUEST_ROBOTS_PENDING,
-    REQUEST_ROBOTS_SUCCESS,
-    REQUEST_ROBOTS_FAILED
-   } from '../constants';
-// import * as Action from '../api/api';
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+  } from '../constants';
 
 describe('Redux Actions', () => {
 
-    const ctx = 'Hello'
+  const ctx = 'Hello'
+  const expectedAction ={
+    type: CHANGE_SEARCHFIELD,
+    payload: ctx
+  }
+  
+  const url ='https://jsonplaceholder.typicode.com/users'
+  
+  it('should create an action to search robots', () => {
+    expect(Action.setSearchField(ctx)).toEqual(expectedAction)
+  })
+
+  it('handles requesting robots API', () => {
+    const store = mockStore();
+    store.dispatch(Action.requestRobots())
+    const action =store.getActions();
+    
     const expectedAction ={
-        type: CHANGE_SEARCHFIELD,
-        payload: ctx
+        type: REQUEST_ROBOTS_PENDING
     }
-    
-    const url ='https://jsonplaceholder.typicode.com/users'
-    
-    it('should create an action to search robots', () => {
-        expect(Action.setSearchField(ctx)).toEqual(expectedAction)
-    })
 
-    it('handles requesting robots API', () => {
-        const store = mockStore();
-        store.dispatch(Action.requestRobots())
-        const action =store.getActions();
-        const expectedAction ={
-            type: REQUEST_ROBOTS_PENDING
-        }
-
-        expect(action[0]).toEqual(expectedAction)
-    })
+    expect(action[0]).toEqual(expectedAction)
+  })
 })
